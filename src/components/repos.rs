6f1e172358509d0b31ac0e91f121053c824f2d60
Gerixmus <gerixmuss@gmail.com>
2026-07-14 01:02:32 +0200

    fix: correct interaction index logic

--- src/components/repos.rs
+++ src/components/repos.rs
@@ -43,7 +43,13 @@ impl Component for Repos {
 
         self.list_state.select(Some(self.counter));
 
-        let items = self.get_filtered_slugs();
+        let items: Vec<String> = self
+            .items
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| self.get_filtered_indexes().contains(i))
+            .map(|(_, r)| r.slug.to_string())
+            .collect();
 
         let title = Line::from(" Repo ".bold());
         let title_bottom = Line::from(format!(" {} of {} ", self.counter + 1, items.len()));
@@ -144,27 +150,30 @@ impl Repos {
     }
 
     fn increment_counter(&mut self) {
-        self.counter = (self.counter + 1) % self.get_filtered_slugs().len();
+        self.counter = (self.counter + 1) % self.get_filtered_indexes().len();
     }
 
     fn decrement_counter(&mut self) {
-        self.counter = (self.counter + self.items.len() - 1) % self.get_filtered_slugs().len();
+        self.counter = (self.counter + self.items.len() - 1) % self.get_filtered_indexes().len();
     }
 
     fn interact(&mut self) {
+        let repo = &self.items[self.get_filtered_indexes()[self.counter]];
         Command::new(&self.editor)
-            .arg(&self.items[self.counter].path)
+            .arg(&repo.path)
             .output()
             .expect("failed to execute process");
         self.exit = true;
     }
 
-    fn get_filtered_slugs(&self) -> Vec<String> {
-        let filter = &self.search.to_lowercase();
+    fn get_filtered_indexes(&self) -> Vec<usize> {
+        let filter = self.search.to_lowercase();
+
         self.items
             .iter()
-            .filter(|r| r.slug.to_lowercase().starts_with(filter))
-            .map(|r| r.slug.clone())
+            .enumerate()
+            .filter(|(_, r)| r.slug.to_lowercase().starts_with(&filter))
+            .map(|(i, _)| i)
             .collect()
     }
 }
